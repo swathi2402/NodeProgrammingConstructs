4feaefa85a5a838d41a9336f04802d674981dc01
Swathi <swathi.h@perfios.com>
2021-10-06 20:59:48 +0530

    ADDED: Given a number printing corresponding week using switch case

---
+++ Case Statements/DisplayWeek.js
@@ -0,0 +1,33 @@
+let readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question("Enter number:", numberInput => {number = numberInput;
+    switch (Number (number)) {
+    case 1: 
+        console.log("Sunday");
+        break;
+    case 2: 
+        console.log("Monday");
+        break;
+    case 3: 
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5: 
+        console.log("Thursday");
+        break;
+    case 6: 
+        console.log("Friday");
+        break;
+    case 7: 
+        console.log("Saturday");
+        break;
+    default: 
+        console.log("Invalid input");
+    }
+    readline.close();
+});
