60c9bc77dc3f25738cc70e967e75d717b0cb3b41
Swathi <swathi.h@perfios.com>
2021-10-06 20:26:10 +0530

    ADDED: Performing arithmetic operations on 3 numbers and finding min and max

---
+++ If-Else If-Else/ArithmeticOperations.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 15;
+let c = 12;
+
+number1= a + b * c;
+number2 = a % b + c;
+number3 = c + a / b;
+number4 = a * b + c;
+
+console.log("Minimum number: "+ Math.min(number1, number2, number3, number4));
+console.log("Maximum number: "+ Math.max(number1, number2, number3, number4));
+
