6e2d347b60b9644368184404d28ba919d788d3fb
Swathi <swathi.h@perfios.com>
2021-10-06 21:38:30 +0530

    MODIFIED: Given a range printing whether a number is prime or not

--- Foor loop/PrimeNumber.js
+++ Foor loop/PrimeNumber.js
@@ -4,15 +4,21 @@ let readline = require('readline').createInterface({
 })
 
 let flag = 0;
-readline.question("Enter number:", numberInput => {number = numberInput;
-    for(let index = 2; index < number; index++){
-        if(number % index == 0){
-            console.log("Not Prime")
-            flag = 1;
-            break;
+readline.question("Enter lower limit:", lowerInput => {
+    readline.question("Enter upper limit:", upperInput => {
+        for(let i = lowerInput; i <= upperInput; i++) {
+            for(let j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    console.log(i + " is not a Prime")
+                    flag = 1;
+                    break;
+                }
+                flag = 0;
+            }
+            if(flag == 0) {
+                console.log(i + " is a Prime");
+            }
+            readline.close();
         }
-    }
-    if(flag == 0)
-        console.log("Prime");
-    readline.close();
+    });
 });
