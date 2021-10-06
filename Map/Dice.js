5fcd3bb86f11434b4552b52ea62f5f36a76f88dd
Swathi <swathi.h@perfios.com>
2021-10-07 05:27:53 +0530

    MODIFIED: Repeating rolling dice to get results

--- Map/Dice.js
+++ Map/Dice.js
@@ -1,2 +1,5 @@
-let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
-console.log("Dice number: ", diceNumber);
+let numberOfRolls = 11;
+for(let i = 0; i < numberOfRolls; i++){
+    diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+    console.log(diceNumber);
+} 
