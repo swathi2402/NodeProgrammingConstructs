4163344ec0a9f34b0f6bdff422fc6f7438f262e8
Swathi <swathi.h@perfios.com>
2021-10-06 19:04:25 +0530

    ADDED: Addition of 2 randomly generated dice number

--- Sequence/SequenceProblems.js
+++ Sequence/SequenceProblems.js
@@ -3,3 +3,7 @@ console.log("Single digit: ", singleDigit);
 
 let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
 console.log("Dice number: ", diceNumber);
+
+let firstDiceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+let secondDiceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("Addition of dice numbers", firstDiceNumber, "+", secondDiceNumber, ":", firstDiceNumber + secondDiceNumber);
