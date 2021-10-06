cc5bec8a52b6ffc8ed0840ffb37619936378b920
Swathi <swathi.h@perfios.com>
2021-10-06 18:57:51 +0530

    ADDED: Random function to get random number for dice

--- Sequence/SequenceProblems.js
+++ Sequence/SequenceProblems.js
@@ -1,2 +1,5 @@
 let singleDigit = Math.floor(Math.random() * 10) % 10;
 console.log("Single digit: ", singleDigit);
+
+let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("Dice number: ", diceNumber);
