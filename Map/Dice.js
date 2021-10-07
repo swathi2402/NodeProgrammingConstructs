354405cfe78485050801aa8d9a0fb98db11e1bf3
Swathi <swathi.h@perfios.com>
2021-10-07 05:36:50 +0530

    MODIFIED: Stop rolling a dice untill any count reaches 10

--- Map/Dice.js
+++ Map/Dice.js
@@ -1,7 +1,14 @@
 let numberOfRolls = 11;
 let dieRollMap = new Map();
-for(let i = 0; i < numberOfRolls; i++){
+for(let i = 1; i<=6; i++) {
+    dieRollMap.set(i, 0);
+}
+
+while(true) {
     diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
-    dieRollMap.set(i, diceNumber);
+    let count = dieRollMap.get(diceNumber);
+    dieRollMap.set(diceNumber, count + 1);
+    if (count + 1 == 10)
+        break;
 } 
 console.log(dieRollMap);
